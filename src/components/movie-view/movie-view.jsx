5dfbc3e0694555a1169dccf32a6b9feb4d16c291
jamkerr <kerr0088@gmail.com>
2022-11-14 12:17:52 +0100

    3.3: Add interactive movie card - Clicking movie card displays movie view - Back button returns to main view - Updated movie info, images currently don't work

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 export class MovieView extends React.Component {
     render() {
-        const { movieData } = this.props;
+        const { movieData, onBackClick } = this.props;
         return <div className='movie-view'>
                 <div className='movie-title'>{movieData.Title}</div>
                 <div className='movie-description'>{movieData.Description}</div>
                 <div className='movie-image'>
                     <img src={movieData.ImagePath}/>
-                </div>          
+                </div>
+                {/* If back button is clicked, pass null via onBackClick to selectedMovie state, which switches from MovieView to MovieCard  */}
+                <button onClick={() => { onBackClick(null); }}>Back</button>          
             </div>;
     }
 }
