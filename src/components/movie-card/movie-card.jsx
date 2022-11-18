5aaac61c14828f694b6764b007dfe2036354bc6e
jamkerr <kerr0088@gmail.com>
2022-11-18 12:11:23 +0100

    3.5: Migrate Movie Card to React Bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
     render() {
         const { movieData, onMovieClick } = this.props;
-        return <div className='movie-card' onClick={() => { onMovieClick(movieData); }}>{movieData.Title}</div>;
+
+        return (
+            <Card bg='secondary' className='movie-card'>
+                <Card.Body>
+                    <Card.Title>{movieData.Title}</Card.Title>
+                    <Card.Text>{movieData.Description}</Card.Text>
+                    <Button onClick={() => onMovieClick(movieData)} variant="link">Open</Button>
+                </Card.Body>
+            </Card>
+        );
     }
     
 }
