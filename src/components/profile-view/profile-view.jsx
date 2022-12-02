89b91a05be34efecf4c41d207f9cd7359fe792c6
jamkerr <kerr0088@gmail.com>
2022-12-02 13:23:47 +0100

    3.6: Allow account to be deleted

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -30,14 +30,48 @@ export function ProfileView({username}) {
         }
     }, []);
 
+    const handleDelete = async () => {
+        let token = localStorage.getItem('token');
+        if (username && token) {
+            let confirmDelete = confirm('Are you sure you want to permanently delete your account?');
+            if (!confirmDelete) return;
+
+            try {
+                await axios.delete(`https://ymdeebee.herokuapp.com/users/${username}`, {
+                    headers: { Authorization: `Bearer ${token}` }
+                });
+                alert('Your account was permanently deleted.');
+                localStorage.clear();
+                window.open('/', '_self');
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+
     return (
         <Container className='profile-container d-flex justify-content-center align-items-center'>
-            <Row className='margin-auto'>
+            <Row xs={1} >
+                <Row>
+                    <h2>Your Profile</h2>
+                </Row>
                 <Col>
                     <Card bg='secondary' className='profile-card'>
                         <Card.Body>
-                            <Card.Title>{username}</Card.Title>
-                            <Card.Text>{userData.Email}</Card.Text>
+                            <Card.Title>Name: {username}</Card.Title>
+                            <Card.Text>Email: {userData.Email}</Card.Text>
+                            <Card.Text>Birthday: {userData.Birth_Date}</Card.Text>
+                            {console.log(userData)}
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Row>
+                    <h2>Delete Account</h2>
+                </Row>
+                <Col>
+                    <Card bg="dark">
+                        <Card.Body>
+                            <Button variant="danger" onClick={() => { handleDelete() }} >Delete Account</Button>
                         </Card.Body>
                     </Card>
                 </Col>
