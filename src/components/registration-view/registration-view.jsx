bc9924257d3e0c6840044de9098aaf233a315c92
jamkerr <kerr0088@gmail.com>
2022-12-02 13:23:08 +0100

    Allow new user registration

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -9,14 +11,16 @@ import Card from 'react-bootstrap/Card';
 
 import './registration-view.scss';
 
-export function RegistrationView(props) {
+export function RegistrationView() {
     const [ username, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
     const [ email, setEmail ] = useState('');
+    const [ birthday, setBirthday ] = useState('');
     // Declare hook for each input
     const [ usernameErr, setUsernameErr ] = useState('');
     const [ passwordErr, setPasswordErr ] = useState('');
     const [ emailErr, setEmailErr ] = useState('');
+    const [ birthdayErr, setBirthdayErr ] = useState('');
 
 
     // validate user inputs
@@ -32,8 +36,8 @@ export function RegistrationView(props) {
         if (!password) {
             setPasswordErr('You need a password');
             isReq = false;
-        } else if (password.length < 6) {
-            setPasswordErr('Password must be at least 6 characters long');
+        } else if (password.length < 8) {
+            setPasswordErr('Password must be at least 8 characters long');
             isReq = false;
         }
         if (!email) {
@@ -43,15 +47,30 @@ export function RegistrationView(props) {
             setEmailErr('You sure that email is correct?');
             isReq = false;
         }
+        if (!birthday) {
+            setBirthdayErr('We need to check how old you are');
+            isReq = false;
+        }
 
         return isReq;
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const isReq = validate();
         if (isReq) {
             /* Send a request to the server for authentication */
+            try {
+                const response = await axios.post(`https://ymdeebee.herokuapp.com/users`, {
+                    Username: username,
+                    Password: password,
+                    Email: email,
+                    Birth_Date: birthday
+                })
+                window.open('/', '_self');
+            } catch (error) {
+                console.log(error);
+            }
         }
     };
 
@@ -89,9 +108,21 @@ export function RegistrationView(props) {
                                         type="email"
                                         value={email}
                                         onChange={e => setEmail(e.target.value)}
+                                        placeholder="Enter your email address"
                                         required  
                                     />
                                     {emailErr && <p>{emailErr}</p>}
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Birthday:</Form.Label>
+                                    <Form.Control
+                                        type="date"
+                                        value={birthday}
+                                        onChange={e => setBirthday(e.target.value)}
+                                        placeholder="Enter your birthday"
+                                        required  
+                                    />
+                                    {birthdayErr && <p>{birthdayErr}</p>}
                                 </Form.Group>
                                 <Button className='mt-2' variant="info" type="submit" onClick={handleSubmit}>Create account</Button>
                             </Form>
